82b59acae2db90223c1489796b7255d0de20ba2b
CarloSalaris <carloa.salaris@gmail.com>
2023-05-22 16:44:02 +0200

    Bonus 1 aggiungi task pressing ENTER key

--- js/main.js
+++ js/main.js
@@ -12,7 +12,11 @@ MILESTONE 2
 Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa, il task viene rimosso dalla lista.
 
 MILESTONE 3
-Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo task, che quindi viene aggiunto alla lista dei task esistenti. */
+Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo task, che quindi viene aggiunto alla lista dei task esistenti.
+
+BONUS
+1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il task alla lista
+2- cliccando sul testo dell’item, invertire il valore della proprietà done del task corrispondente (se done era uguale a false, impostare true e viceversa) */
 
 const{createApp} = Vue
 createApp({
